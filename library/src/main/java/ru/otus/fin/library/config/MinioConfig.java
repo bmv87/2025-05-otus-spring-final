47b424d0362ef25590159a0287a3ef88e74c160c
BMV <venerasmith@mail.ru>
2025-11-13 01:53:55 +0300

    feat: env vars + minio config

---
+++ library/src/main/java/ru/otus/fin/library/config/MinioConfig.java
@@ -0,0 +1,21 @@
+package ru.otus.fin.library.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Autowired
+    MinioProperties minioProperties;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getUrl())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+    }
+}
